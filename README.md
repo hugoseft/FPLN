99cd665c105b298a4bbfcff15693a9089ec81fcb
ander.asteinza <ander.asteinza@udc.es>
2026-02-27 10:09:50 +0100

    Create EvaluationWorPieceBPE File

--- README.md
+++ README.md
@@ -1,1 +1,6 @@
 # FPLN
+
+
+
+Se pide también realizar
+una discusión sobre las diferencias observadas. (entre la salida de entrenamiento y prueba)
--- Segmentacion.py
+++ Segmentacion.py
@@ -225,13 +225,9 @@ class TokenizadorBPE:
     
 
 if __name__ == "__main__":
-    with open("training_sentences.txt", "r", encoding="utf-8") as f:
-        train_lines = [line.strip() for line in f if line.strip()]
     
     with open("test_sentences.txt", "r", encoding="utf-8") as f:
         test_lines = [line.strip() for line in f if line.strip()]
-
-    corpus_train_completo = " ".join(train_lines)
 
     print("=== RESULTADOS EN TEST_SENTENCES.TXT ===\n")
 
@@ -251,27 +247,5 @@ if __name__ == "__main__":
     for line in test_lines:
         print(f"Input: '{line}' -> Tokens: {Token_clas_superv(line)}")
 
-    tamanos_vocabulario = [100, 150, 200]
 
-    for size in tamanos_vocabulario:
-        print(f"\n=========================================")
-        print(f"   EVALUANDO TAMAÑO DE VOCABULARIO: {size}")
-        print(f"=========================================")
-        
-        # WordPiece
-        print(f"\n--- WordPiece (Vocab: {size}) ---")
-        wp_tokenizer = TokenizadorWordPiece(vocab_size=size)
-        wp_tokenizer.train(corpus_train_completo)
-        print(f"Vocabulario WordPiece ({size}):\n{wp_tokenizer.vocab}\n")
-        
-        for line in test_lines:
-            print(f"Input: '{line}' -> Tokens: {wp_tokenizer.tokenize(line)}")
-
-        print(f"\n--- BPE (Vocab: {size}) ---")
-        bpe_tokenizer = TokenizadorBPE(vocab_size=size)
-        bpe_tokenizer.train(corpus_train_completo)
-        print(f"Vocabulario BPE ({size}):\n{bpe_tokenizer.vocab}\n")
-        
-        for line in test_lines:
-            print(f"Input: '{line}' -> Tokens: {bpe_tokenizer.tokenize(line)}")
 
